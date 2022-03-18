1158a737f0c21a62ff979b24b2a1b5b026d65131
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-03-18 20:38:35 +0100

    feat: create EntityBase

---
+++ src/Incremental.Common.Entity/EntityBase.cs
@@ -0,0 +1,39 @@
+﻿namespace Incremental.Common.Entity;
+
+public abstract class EntityBase
+{
+    /// <summary>
+    /// Id of the entity.
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Date of creation of the entity.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    
+    /// <summary>
+    /// Optional creator of the entity.
+    /// </summary>
+    public Guid? CreatedBy { get; set; }
+    
+    /// <summary>
+    /// Date of last update of the entity
+    /// </summary>
+    public DateTime LastUpdatedAt { get; set; }
+    
+    /// <summary>
+    /// Optional last updater of the entity.
+    /// </summary>
+    public Guid? LastUpdatedBy { get; set; }
+    
+    /// <summary>
+    /// Optional date of deletion.
+    /// </summary>
+    public DateTime? DeletedAt { get; set; }
+    
+    /// <summary>
+    /// Optional deleter of the entity.
+    /// </summary>
+    public Guid? DeletedBy { get; set; }
+}
